4b48c0ea4e7b62550adce0b23c516002103153df
Lyra <lyra2108@gmail.com>
2015-03-06 08:49:35 +0100

    Add , betweeen names and include pdf file in github

--- OpenMP/Loesungen/source/conf.py
+++ OpenMP/Loesungen/source/conf.py
@@ -198,7 +198,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'HPC.tex', 'HPC Übungen',
-   'Sarah Blume Sebastian Rieger', 'howto'),
+   'Sarah Blume, Sebastian Rieger', 'howto'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
